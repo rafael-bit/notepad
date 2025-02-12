ccd27abfbdad99876090724d7d915f303a00fd14
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2025-02-12 15:35:02 -0300

    style: underline in 'click here'

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -11,7 +11,7 @@ export default function Banner() {
 		showBanner && (
 			<div className="w-full h-12 bg-gradient-to-r from-green via-blueLight to-greenMedium flex items-center justify-center">
 				<MdClose className="absolute translate-y-0 right-5 cursor-pointer" size={20} onClick={() => setShowBanner(false)} />
-				<p className="flex">Unique opportunities! Take advantage of our exclusive offers waiting for you!<Link href="/plans" className="flex items-center gap-1">&nbsp; Click Here <MdArrowForward /></Link></p>
+				<p className="flex">Unique opportunities! Take advantage of our exclusive offers waiting for you!<Link href="/plans" className="flex items-center gap-1 under">&nbsp; <span className="underline">Click Here</span><MdArrowForward /></Link></p>
 			</div>
 		)
 	)
