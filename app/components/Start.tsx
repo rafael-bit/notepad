9db8b9c877623fd3d521e280fe521e5974cd5511
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2025-02-12 16:33:14 -0300

    feat: add start section

---
+++ app/components/Start.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Start() {
+	return (
+		<section className="mt-10 w-full container mx-auto flex flex-col items-center">
+			<div className="w-[90%] flex flex-col items-center p-16 rounded-lg bg-gradient-to-r from-green via-greenMedium to-blueLight">
+				<h1 className="text-5xl font-bold text-neutral-200">Take note with us !</h1>
+				<p className="w-[70%] text-center mt-3 text-neutral-300">Enhance your notes with us and unlock your full potential, making every day more organized, productive, and impactful!</p>
+				<Button className="mt-7 overflow-hidden rounded-full bg-neutral-900 px-7 py-[0.875rem] transition-all duration-300 hover:bg-neutral-800 hover:text-white hover:opacity-90 flex items-end pt-[2.4rem]">
+					<Image
+						src={'/logo.png'}
+						alt="logo"
+						width={30}
+						height={30}
+					/>
+					Get Started with your notes
+				</Button>
+			</div>
+		</section>
+	)
+}
