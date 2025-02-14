a4bc2210372452c7595e25c8f89045770a19b87e
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2025-02-14 10:53:54 -0300

    feat: add protected the dashboard route

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<main className="flex justify-center items-center min-h-screen w-full bg-gray-100">{children}</main>
+		<main className="flex justify-center items-center min-h-screen w-full bg-neutral-900">
+			{children}
+		</main>
 	)
 }
